34d1bfa340fc978f89ccbd6ee38950a91b5297cd
Ben Sapirstein <ben.sapirstein@gmail.com>
2022-12-24 15:39:49 +0200

    Data Ingestion
    ETL process implementation

--- src/data/data_utils.py
+++ src/data/data_utils.py
@@ -1,3 +1,9 @@
+
+import subprocess
+
 def parse_list(string: str) -> list:
     """Convert a string representation of a list to a list."""
     return [s.strip("''") for s in string.strip('[]').split(', ')]
+
+def normalize_ufo(path_to_ufo):
+    subprocess.run(['psfnormalize', path_to_ufo.replace(' ', '\ ')])
