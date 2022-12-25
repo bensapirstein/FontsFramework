e2f1b080187567b8e0a077d69cf323dd7bf5d9bc
Ben Sapirstein <ben.sapirstein@gmail.com>
2022-12-25 23:41:11 +0200

    Glyphs extraction

--- src/data/data_utils.py
+++ src/data/data_utils.py
@@ -1,6 +1,19 @@
 from defcon import Font
 import extractor
+import numpy as np
 from ufo2ft import compileOTF
+from fontTools.pens.cairoPen import CairoPen
+from fontTools.pens.statisticsPen import StatisticsPen
+from fontTools.svgLib.path import SVGPath
+from fontTools.pens.svgPathPen import SVGPathPen
+from fontTools.misc.transform import Identity
+
+import cairo
+
+svg_format="""<svg>
+  <path d="%s" />
+  Sorry, your browser does not support inline SVG.
+</svg>"""
 
 def parse_list(string: str) -> list:
     """Convert a string representation of a list to a list."""
@@ -33,3 +46,48 @@ def ufo_to_ttf(ufo_file_path, ttf_file_path):
     ufo = Font(ufo_file_path)
     otf = compileOTF(ufo)
     otf.save(ttf_file_path)
+
+def glyph_to_svg_path(glyph):
+    pen = SVGPathPen(glyph.getParent())
+    glyph.draw(pen)
+    return pen.getCommands()
+
+def glyph_stats(glyph):
+    pen = StatisticsPen(glyph.font)
+    glyph.draw(pen)
+    return pen.meanX, pen.meanY, pen.stddevX, pen.stddevY
+
+def transform_svg(svg, tx, ty, sx, sy):
+    svgPath = SVGPath.fromstring(svg_format % svg, Identity.scale(sx, sy).translate(tx, ty))
+    pen = SVGPathPen(None)
+    svgPath.draw(pen)
+    return pen.getCommands()
+
+def normalize_svg(svg, meanX, meanY, stddevY):
+    return transform_svg(svg, -meanX, -meanY, np.abs(1/stddevY), np.abs(1/stddevY))
+
+def normalize_glyph(glyph):
+    return transform_svg(glyph["svg"], -glyph["meanX"], -glyph["meanY"],
+                        np.abs(1/glyph["stddevX"]), np.abs(1/glyph["stddevY"]))
+
+def glyph_to_img(glyph, outpath):
+    # Create a Cairo context and pen
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 1000, 1000)
+    context = cairo.Context(surface)
+    context.set_source_rgb(1, 1, 1)
+    context.paint()
+
+    pen = CairoPen(glyph.font, context)
+
+    context.set_source_rgb(0, 0, 0)
+
+    context.set_line_width(20)
+    context.translate(glyph.width, 0)
+    #context.scale(0.2, -0.2)
+    # Draw the glyph using the pen
+    glyph.draw(pen)
+
+    context.stroke()
+
+    # Save the image to a file
+    surface.write_to_png(f"glyphs/{glyph.name}.png")
