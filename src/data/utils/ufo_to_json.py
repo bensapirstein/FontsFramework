e528504ecffba0a1424fc66c8c705dd2fa59ba6f
Ben Sapirstein <ben.sapirstein@gmail.com>
2023-01-07 16:24:28 +0200

    implemented conversion from ufo to json.
    simple working example with mongoDB

--- src/data/utils/ufo_to_json.py
+++ src/data/utils/ufo_to_json.py
@@ -1,11 +1,87 @@
 import json
 import plistlib
 import os
-from fontTools.ufoLib import glifLib
+
+import xml.etree.ElementTree as ET
+import json
+
+
+def glif_to_json(glif_xml_string: str) -> dict:
+    # parse the XML string
+    root = ET.fromstring(glif_xml_string)
+
+    # create a dictionary to hold the JSON object
+    glyph = {}
+
+    # add all attributes of the root element to the JSON object
+    for attr, value in root.attrib.items():
+        glyph[attr] = value
+
+    # add the advance element to the JSON object
+    advance = root.find('advance')
+    if advance is not None:
+        glyph['advance'] = {
+            'width': advance.attrib['width'],
+            'height': advance.attrib.get('height', 0)
+        }
+
+    # add the unicode elements to the JSON object
+    glyph['unicode'] = []
+    for unicode_elem in root.findall('unicode'):
+        glyph['unicode'].append({'hex': unicode_elem.attrib['hex']})
+
+    # add the note element to the JSON object
+    note = root.find('note')
+    if note is not None:
+        glyph['note'] = note.text
+
+    # add the image element to the JSON object
+    image = root.find('image')
+    if image is not None:
+        glyph['image'] = {}
+        for attr, value in image.attrib.items():
+            glyph['image'][attr] = value
+
+    # add the guideline elements to the JSON object
+    glyph['guideline'] = []
+    for guideline in root.findall('guideline'):
+        guideline_dict = {}
+        for attr, value in guideline.attrib.items():
+            guideline_dict[attr] = value
+        glyph['guideline'].append(guideline_dict)
+
+    # add the outline element to the JSON object
+    outline = root.find('outline')
+    if outline is not None:
+        glyph['outline'] = {
+            'contour': [],
+            'component': []
+        }
+
+        # add the contour elements to the JSON object
+        for contour in outline.findall('contour'):
+            contour_dict = {'point': []}
+            for point in contour.findall('point'):
+                point_dict = {}
+                for attr, value in point.attrib.items():
+                    point_dict[attr] = value
+                contour_dict['point'].append(point_dict)
+            glyph['outline']['contour'].append(contour_dict)
+
+        # add the component elements to the JSON object
+        for component in outline.findall('component'):
+            component_dict = {}
+            for attr, value in component.attrib.items():
+                component_dict[attr] = value
+            glyph['outline']['component'].append(component_dict)
+
+    return glyph
+
 
 def ufo_to_json(ufo_path):
     # Read in all plist files and convert to dictionaries
-    plist_files = ['metainfo.plist', 'fontinfo.plist', 'groups.plist', 'kerning.plist', 'lib.plist', 'layercontents.plist']
+    plist_files = ['metainfo.plist', 'fontinfo.plist', 'groups.plist', 'kerning.plist',
+                   'lib.plist', 'layercontents.plist', 'glyphs/contents.plist', 'glyphs/layerinfo.plist']
     plist_data = {}
     for plist_file in plist_files:
         if not os.path.exists(f'{ufo_path}/{plist_file}'):
@@ -15,23 +91,23 @@ def ufo_to_json(ufo_path):
 
     # Read in GLIF files and convert to dictionaries
     glyph_data = {}
-    for glyph_name in os.listdir(f'{ufo_path}/glyphs'):
+
+    for glyph_name, glif_fname in plist_data['glyphs/contents.plist'].items():
         # Read in GLIF file and convert to dictionary
-        with open(f'{ufo_path}/glyphs/{glyph_name}', 'r') as f:
-            glyph_data[glyph_name] = f.read()
+        with open(f'{ufo_path}/glyphs/{glif_fname}', 'r') as f:
+            glyph_data[glyph_name] = glif_to_json(f.read())
+
+    if os.path.exists(f'{ufo_path}/features.fea'):
+        with open(f'{ufo_path}/features.fea', "rb") as f:
+            plist_data['features.fea'] = f.read()
+
 
     # Combine all data into a single dictionary
     ufo_data = {
         **plist_data,
-        'glyph_data': glyph_data
+        'glyphs': glyph_data
     }
 
     # Convert UFO data to JSON
     ufo_json = json.dumps(ufo_data)
     return ufo_json
-
-# Example usage
-ufo_path = '../../../data/processed/fonts/UFO/Alef/Alef-Regular.ufo'
-ufo_json = ufo_to_json(ufo_path)
-with open('ufo.json', 'w') as f:
-  f.write(ufo_json)
